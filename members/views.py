3acdc268d76e67223a4f3e4699466137a47ecd89
Aleksy <aslepaugo@gmail.com>
2023-03-16 08:35:16 +0100

    Add custom user model and login form.

--- members/views.py
+++ members/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import redirect, render
 from members.forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def login_user(request):
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Email or password is incorrect')
+
     context = {}
     return render(request, 'login.html', context)
 
@@ -18,3 +31,8 @@ def register_user(request):
 
     context = {'form': form}
     return render(request, 'register.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
