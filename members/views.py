88986bd5b6c220d82b7d18bf9399b6ec24cb5898
Aleksy <aslepaugo@gmail.com>
2023-03-17 08:17:21 +0100

    Add required login foraccount view

--- members/views.py
+++ members/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import redirect, render
 from members.forms import CreateUserForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def login_user(request):
-
+    next_url = request.GET.get("next", '/')
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
         user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
-            return redirect('/')
+            return redirect(next_url)
         else:
             messages.info(request, 'Email or password is incorrect')
 
@@ -38,6 +39,7 @@ def logout_user(request):
     return redirect('login')
 
 
+@login_required(login_url='login')
 def profile(request):
     context = {}
     return render(request, 'profile.html', context)
