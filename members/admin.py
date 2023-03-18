4b911be27ee40647518446326d19c5916be9e5af
mavel <p.m.berdnikov@gmail.com>
2023-03-18 18:54:01 +0300

    add Subscription model

--- members/admin.py
+++ members/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from members.models import CustomUser
+from members.models import CustomUser, Subscription
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['username', 'email',]
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    pass
--- members/models.py
+++ members/models.py
@@ -4,3 +4,22 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+
+
+class Subscription(models.Model):
+    user = models.OneToOneField(
+        'CustomUser',
+        verbose_name='пользователь',
+        on_delete=models.CASCADE,
+    )
+    allergies = models.ManyToManyField(
+        'recipes.Allergy',
+        verbose_name='аллергии',
+    )
+    meals = models.ManyToManyField(
+        'recipes.MealType',
+        verbose_name='типы блюд',
+    )
+
+    def __str__(self):
+        return self.user.username
