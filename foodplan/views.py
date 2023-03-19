6d69fba635b50debf3a4dbd78055009be8eddb2a
mavel <p.m.berdnikov@gmail.com>
2023-03-19 18:42:26 +0300

    add links to preview recipes from index

--- foodplan/views.py
+++ foodplan/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from recipes.models import Recipe, MealGroup
+from recipes.models import Recipe
 
 
 def index(request):
@@ -8,16 +8,9 @@ def index(request):
         'Модный и полезный ужин',
         'Интересно, просто и супер вкусно',
         'Понятное всей семье, яркое на вкус',
-        'Летнее удовольствие',
         'Который все пекут повторно',
-
+        'Летнее удовольствие',
     ]
-    recipes = []
-    all_recipes = Recipe.objects.all()
-    meal_order = [MealGroup.MAIN, MealGroup.DESSERT, MealGroup.BREAKFAST]
-    for meal in meal_order:
-        recipes.extend(
-            all_recipes.filter(category__pk=meal)[:2]
-        )
+    recipes = Recipe.objects.filter(pk__lte=5)
     context = {'recipes_labels': zip(recipes, labels)}
     return render(request, 'index.html', context)
