4ab023e40bc30f5727329705e499fd9058950e8d
Aleksy <aslepaugo@gmail.com>
2023-03-19 16:48:25 +0100

    Add subscription form

--- members/views.py
+++ members/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth import authenticate, login, logout, update_session_auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from members.models import CustomUser, Subscription
+from members.models import Subscription
+from recipes.models import MenuCategory, Allergy, MealType
 
 
 def login_user(request):
@@ -80,5 +81,37 @@ def profile(request):
 
 
 def subscription(request):
-    context = {}
+    if request.method == 'POST':
+        for i in request.POST.items():
+            print(i)
+        subscription = Subscription()
+        subscription.user = request.user
+        subscription.duration = request.POST.get('duration')
+        subscription.person_count = request.POST.get('person_count')
+        foodtype = request.POST.get('foodtype')
+
+        if foodtype == 'low':
+            subscription.menu_category = MenuCategory.objects.get(title='Vegan')
+        elif foodtype == 'veg':
+            subscription.menu_category = MenuCategory.objects.get(title='Lowcarb')
+        elif foodtype == 'keto':
+            subscription.menu_category = MenuCategory.objects.get(title='Keto')
+        else:
+            subscription.menu_category = MenuCategory.objects.get(title='Classic')
+        subscription.save()
+
+        if int(request.POST.get('breakfast')) == 0:
+            subscription.meals.add(MealType.objects.get(label='Breakfast'))
+        if int(request.POST.get('dinner')) == 0:
+            subscription.meals.add(MealType.objects.get(label='Dinner'))
+        if int(request.POST.get('dessert')) == 0:
+            subscription.meals.add(MealType.objects.get(label='Dessert'))
+
+        for key, value in request.POST.items():
+            if key.startswith('allergy'):
+                subscription.allergies.add(Allergy.objects.get(id=int(value)))
+
+        return redirect('profile')
+
+    context = {'allergies': Allergy.objects.all()}
     return render(request, 'order.html', context)
